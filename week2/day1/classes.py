2b2a1dea0c5dfe558042cfcfbb923f60ab0dbb19
Jessika Adams <jssklynn@gmail.com>
2022-11-21 09:48:04 -0700

    Update classes.py
    Exercise

--- week2/day1/classes.py
+++ week2/day1/classes.py
@@ -1,1 +1,27 @@
-print("Hello World classes")
+class Pet:
+    def __init__(self,name,owner,noise):
+        self.name = name
+        self.owner = owner
+        self.noise = noise
+    def whoIsMyOwner(self):
+        print(f"{self.name}'s owner is {self.owner}.")
+    def whatIsMyName(self):
+        print(f"This pet's name is {self.name}.")
+    def whatSound(self):
+        print(f"{self.name} says {self.noise}!")
+
+Leia = Pet("Leia","Jessika","Bark")
+Gimpy = Pet("Gimpy","Jessika","Meow")
+Luci = Pet("Lucipurr","Jessika","Meow")
+
+Leia.whatIsMyName()
+Leia.whoIsMyOwner()
+Leia.whatSound()
+
+Gimpy.whatIsMyName()
+Gimpy.whoIsMyOwner()
+Gimpy.whatSound()
+
+Luci.whatIsMyName()
+Luci.whoIsMyOwner()
+Luci.whatSound()
