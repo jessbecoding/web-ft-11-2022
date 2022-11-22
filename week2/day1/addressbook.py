ac2d6bccb61a2760985cdd5b0aa98431d8368b48
Jessika Adams <jssklynn@gmail.com>
2022-11-22 08:54:47 -0700

    Looping back to menu
    Looping back to menu

--- week2/day1/addressbook.py
+++ week2/day1/addressbook.py
@@ -2,7 +2,7 @@
 #   I should be able to view all the phone numbers I have created up to that point 
 #   I should be able to add phone numbers
 #   I should be able to see the names of the people attached to each phone number
-# I should be able to delete a specific phone number
+#   I should be able to delete a specific phone number
 #   I should be able to add phone numbers until I specify a stop and then the game will quit
 # The phone number needs to be in this format  111-111-1111
 #   You should use classes
@@ -40,11 +40,16 @@ def phoneBookApp():
                     for i, entry in enumerate(ContactList):
                         if entry.name == name:
                             del ContactList[i]
-                            break
                             print("All Done!")
                 if(choice == "N", "n"):
-                    break
-                    # I want to prompt them again to enter a name, how do I recall back to that?
+                    name = input("Which contact would you like to delete? \n")
+                    if(name == name):
+                        choice = input("Are you sure you want to delete? Y for YES or N for NO \n")
+                        if(choice == "Y", "y"):
+                            for i, entry in enumerate(ContactList):
+                                if entry.name == name:
+                                    del ContactList[i]
+                                    print("All Done!")
             if(name != name):
                 print("That contact isn't found.")
         if(choice == "5"):
