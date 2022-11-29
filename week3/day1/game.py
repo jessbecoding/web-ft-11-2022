4f85bcf3690f68607c436fe598b37a97c0caafaa
Jessika Adams <jssklynn@gmail.com>
2022-11-29 13:06:19 -0700

    More reformatting
    this is incredibly disorganized

--- week3/day1/game.py
+++ week3/day1/game.py
@@ -40,7 +40,8 @@ def player_menu():
     menuchoice = input("""
 1. Hero Stats
 2. Items
-3. Quit
+3. Continue
+4. Quit
 """)
     while True:
         if(menuchoice == '1'):
@@ -49,7 +50,7 @@ def player_menu():
         elif(menuchoice == '2'):
             hero_name.displayInventory()
             hero_name.main_menu()
-        elif(menuchoice == '3'):
+        elif(menuchoice == '4'):
             quit
         elif(menuchoice != '1','2','3'):
             print("Please select a valid option")
@@ -71,109 +72,132 @@ def title_type(str):
         sys.stdout.flush()
         time.sleep(random.random()*15.0/typing_speed)
 
-print("Hello, Hero! What is your name?\n")
 hero_name = input()
 
-hero_name = Hero(hero_name,'50','10')
-
-print("We're so glad you're here. Lucipurr has been terrorizing Town and we need your help! Lucipurr is located in her lair at the end of the forest. Please, take this. It will aid you on your journey \n")
-
-hero_name.addItem("Potion")
-
-print("""
+def create_hero(hero_name):
+    hero_name = input("Hello, Hero! What is your name?\n")
+    hero_name = Hero(hero_name,'50','10')
+    print(f"Thank you, {hero_name.name}. We're so glad you're here.\n")
+    print("Lucipurr, the troublemaking cat, has been terrorizing Our Town and we need your help! Lucipurr is located in her lair at the end of the forest. Please, take this. It will aid you on your journey \n")
+    hero_name.addItem("Potion")
+    print("""
     A potion has been added to your inventory.
     """)
-
-print("Now, go! The fate of Town is in your hands. \n")
-
-print("""
+    print("Now, go! The fate of Town is in your hands. \n")
+    print("""
     ----------------------------------
     | ~*Name of Game & Fancy Stuff*~ |
     ----------------------------------
     """)
 
-print("""
-    What would you like to do?
-    1. Head to Lair
-    2. View Main Menu
+def starting_point():
+    startingchoice = input("""You follow the path out of Cloud City toward the dense forest of her outskirts. Through the forest is the way to Lucipurr's Lair. At the entrance to the trees, there's The Last Chance shop. What would you like to do?
+    1. Enter the forest
+    2. Shop at The Last Chance
+    3. View Player Menu
     """)
+    if(startingchoice == '1'):
+        enter area 2
+    if(startingchoice == '2'):
+        # enter area 3
+    elif(startingchoice == '3'):
+        player_menu()
 
-choice = input()
 
-if(choice == "1"):
-    print("""
-You follow road down path. It leads to a dense forest. 
-To your right, there appears to be a small clearing. 
-What would you like to do?
-    """)
-    print("""
-    1. Investigate the clearing
-    2. Continue on to Lair
-    3. View Main Menu
-    """)
-    clearing_choice = input()
-    if(choice == '1'):
-        print("""
-You push your way past some brush and stumble into the clearing, which appears to be a small meadow.
-There is a wooden chest tucked between two trees on the far end. 
-What would you like to do?
-        """)
-        print("""
-        1. Investigate chest
-        2. Go back to path
-        3. View Main Menu
-        """)
-        chest_choice = input()
-        if(chest_choice == '1'):
-            print("""
-You lift open the heavy wooden lid of the chest. 
-Somehow, it seems like the chest is larger on the inside...
-At the bottom, you see a glimmer catching the light from the sun above you.
-A sword?
-""")
-            print("""
-    What would you like to do?
-        1. Take Sword
-        2. Turn back
-        3. View Main Menu
-            """)
-            sword_choice = input()
-            if(sword_choice == '1'):
-                print("""
-You pull it out with ease, despite its size.
-It's like...
-It was meant for you?
-                """)
-            hero_name.addItem("Sword")
-            hero_name.attack + '10'
-            print("You can now use the sword to attack. +10 AP")
-            print("""
-    What would you like to do?
-        1. Continue to Lair
-        2. View Main Menu
-        """)
-        if(chest_choice == '2'):
-            print("You suspiciously eye the chest and determine it must be a mimic. You turn around and leave the meadow to rejoin your previous path.")
-        print("""
-    What would you like to do?
-        1. Continue to Lair
-        2. Main Menu
-            """)
+# Into the forest you go. The light is quickly blocked by the thick canopy of trees above you. Even through this dim light you can see a faint path to the right. What would you like to do?
+#     1. Continue ahead to Lucipurr's Lair
+#     2. Inverstigate the path
+#     3. View Player Menu
+# """)
+#     if(clearingchoice == '1'):
+#         print("""
+# Not trusting this footpath, you forge ahead to the lair. As you walk, you're suddenly abushed by Lucipurr's henchman-- NAME!
+# """)
 
-    if(clearing_choice == '2'):
-        print("You suspiciously eye the chest and determine it must be a mimic. You turn around and leave the meadow to rejoin your previous path.")
-        print("""
-    What would you like to do?
-        1. Continue to Lair
-        2. Main Menu
-            """)
-    if(clearing_choice == '3'):
-        hero_name.main_menu()
+create_hero(hero_name)
 
-elif(choice == '2'):
-    choice = hero_name.main_menu()  
-elif(choice == '3'):
-    quit
+# print("""
+#     What would you like to do?
+#     1. Head to Lair
+#     2. View Main Menu
+#     """)
+
+# choice = input()
+
+# if(choice == "1"):
+#     print("""
+# You follow road down path. It leads to a dense forest. 
+# To your right, there appears to be a small clearing. 
+# What would you like to do?
+#     """)
+#     print("""
+#     1. Investigate the clearing
+#     2. Continue on to Lair
+#     3. View Main Menu
+#     """)
+#     clearing_choice = input()
+#     if(choice == '1'):
+#         print("""
+# You push your way past some brush and stumble into the clearing, which appears to be a small meadow.
+# There is a wooden chest tucked between two trees on the far end. 
+# What would you like to do?
+#         """)
+#         print("""
+#         1. Investigate chest
+#         2. Go back to path
+#         3. View Main Menu
+#         """)
+#         chest_choice = input()
+#         if(chest_choice == '1'):
+#             print("""
+# You lift open the heavy wooden lid of the chest. 
+# Somehow, it seems like the chest is larger on the inside...
+# At the bottom, you see a glimmer catching the light from the sun above you.
+# A sword?
+# """)
+#             print("""
+#     What would you like to do?
+#         1. Take Sword
+#         2. Turn back
+#         3. View Main Menu
+#             """)
+#             sword_choice = input()
+#             if(sword_choice == '1'):
+#                 print("""
+# You pull it out with ease, despite its size.
+# It's like...
+# It was meant for you?
+#                 """)
+#             hero_name.addItem("Sword")
+#             hero_name.attack + '10'
+#             print("You can now use the sword to attack. +10 AP")
+#             print("""
+#     What would you like to do?
+#         1. Continue to Lair
+#         2. View Main Menu
+#         """)
+#         if(chest_choice == '2'):
+#             print("You suspiciously eye the chest and determine it must be a mimic. You turn around and leave the meadow to rejoin your previous path.")
+#         print("""
+#     What would you like to do?
+#         1. Continue to Lair
+#         2. Main Menu
+#             """)
+
+#     if(clearing_choice == '2'):
+#         print("You suspiciously eye the chest and determine it must be a mimic. You turn around and leave the meadow to rejoin your previous path.")
+#         print("""
+#     What would you like to do?
+#         1. Continue to Lair
+#         2. Main Menu
+#             """)
+#     if(clearing_choice == '3'):
+#         hero_name.main_menu()
+
+# elif(choice == '2'):
+#     choice = hero_name.main_menu()  
+# elif(choice == '3'):
+#     quit
 
 
 
