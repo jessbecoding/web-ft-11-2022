a945d20f9c7e35999f0cbf33701a3c5977926514
Jessika Adams <jssklynn@gmail.com>
2022-11-29 20:44:24 -0700

    bedtime
    I can't look at this anymore

--- week3/day1/game.py
+++ week3/day1/game.py
@@ -10,7 +10,8 @@ class Hero:
         print(f"{self.name}'s health is now {self.health}.")
     def lunge(self):
         print(f"{self.name} lunges forward with a powerful thrust.")
-        # Need to put something here where it's like villan health - hero AP
+        lucipurr.health -= self.attack
+        print(f"{lucipurr.name}'s health is now {lucipurr.health}")
         # Maybe a chance to miss?
     def addItem(self,itm):
         self.items.append(itm)
@@ -20,8 +21,10 @@ class Hero:
         self.gold += gold
     def useItem(self,i):
         self.items.remove(i)
-    # def heal(self):
-        # Need something like hero health plus 15 for the potion
+    def addAP(self):
+        self.attack += 50
+    def heal(self):
+        self.health += 50
 
 class Villan:
     def __init__(self,name,health,attack):
@@ -31,6 +34,9 @@ class Villan:
     def villanTakeDamage(self):
         # Need to put something here like villan healkth - hero attack
         print(f"{self.name}'s health is now {self.health}.")
+    def attackFail(self):
+        print("""
+You heave your sturdy shield in front of you, just in the knick of time! Lucipurr's outstretched claws make contact with the shield, buying you time to parry.""")
 
 lucipurr = Villan("Lucipurr",'10', '70')
 
@@ -58,6 +64,29 @@ def displayInventory(hero_name):
     for item in hero_name.items:
         print(item)
     print(f"You have {hero_name.gold} gold.")
+
+def combat_menu(lucipurr,hero_name):
+    combatchoice = input("""
+What would you like to do?
+    1. Block
+    2. Attack
+    3. Use Item
+""")
+    if(combatchoice == '1'):
+        blockitem = "Shield"
+        if blockitem in hero_name.items:
+            if blockitem == "Shield":
+                lucipurr.attackFail()
+            elif blockitem != "Shield":
+                print("You throw your arms over your face, but it's all for naught. All 5 of Lucipurr's sharp claws tear into you. YOU DIED")
+                quit
+    if(combatchoice == '2'):
+        attackitem = "Sword"
+        if attackitem in hero_name.items:
+            if attackitem != "Sword":
+                print("You give it your all as you wind up a powerful punch and unleash it on Luci!")
+                lucipurr.health -= hero_name.attack
+
 
 def player_menu(hero_name):
     menuchoice = input("""
@@ -116,21 +145,17 @@ You follow the path out of Cloud City toward the dense forest of her outskirts. 
     elif(startingchoice == '3'):
         player_menu(hero_name)
 
-def luciLair ():
+def luciLair (lucipurr,hero_name):
     fightchoice = input("""
 You traverse the rugged forest and finally come to a stop outside of Lucipurr's Lair. There is a giant heavy door cracked slightly ajar.
 Would you like to open it? Y or N
 """).upper()
     if(fightchoice == "Y"):
-        blockchoice = input("""
-You slowly open the door to a shadowy figure, waiting for you. Lucipurr turns to meet your gaze. The hatred of nine lifetimes bares into your soul. You have only a brief moment to act! What do you do?
-    1. Block
-    2. Attack
-    3. Use Item
-""")
-
+        print("You slowly widen the opening and step inside. Your eyes adjust to the light for a shadowy figure, perched for an attack. You only have a moment to react!")
+        combat_menu(lucipurr,hero_name)
     if(fightchoice == "N"):
-        hjfdgkjh
+        print("Really?! You're giving up now?! Get back in there!!")
+        luciLair()
 
 def theMoneyPouch(hero_name):
     moneychoice = input("""
@@ -195,6 +220,7 @@ What would you like to do?
                 print("You reach down a full arm's length into the chest and pull the sword from below. Despite its size, you're able to bring it out with ease.")
                 hero_name.addItem("Sword")
                 print("The sword has been added to your inventory.")
+                hero_name.addAP()
                 swordexit = input("""
 What would you like to do?
     1. Go back
@@ -204,7 +230,14 @@ What would you like to do?
                     intoTheForest()
                 elif(swordexit == '2'):
                     player_menu(hero_name)
-
+        elif(chestchoice == '2'):
+            luciLair(lucipurr,hero_name)
+        elif(chestchoice == '3'):
+            intoTheForest()
+    elif(forestchoice == '2'):
+        luciLair(lucipurr,hero_name)
+    elif(forestchoice == '3'):
+        player_menu()
 create_hero(hero_name)
 
 ##check win/loss condiditons##
