07ab049d5a1c6c4fc14f50bb9c95b4d36db3a349
Jessika Adams <jssklynn@gmail.com>
2022-12-12 10:13:58 -0700

    working on calc
    yeesh

--- week4/day4/calc/calc.js
+++ week4/day4/calc/calc.js
@@ -1,6 +1,24 @@
-const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll("operators");
-console.log(numbers)
-for(const button of numbers) {
-    button.addEventListener("click", (e) => (console.log(e.target.innerHTML)));
+const buttons = document.querySelectorAll(".buttons");
+const total = document.querySelector(".total");
+const answer = document.querySelector(".answer");
+
+let numbersToAdd = [];
+
+const calculation = (number) => {
+    numbersToAdd.push(number);
+}
+
+const calcTotal = () => {
+    answer.innerHTML = "";
+    const firstNumber = Number(numbersToAdd[0]);
+    const secondNumber = Number(numbersToAdd[2]);
+    const sum = firstNumber + secondNumber;
+    const newText = sum;
+    answer.append(newText);
+    numbersToAdd = [];
+};
+total.addEventListener("click", calcTotal);
+
+for(const button of buttons) {
+    button.addEventListener("click", (e) => calculation(e.target.innerText));
 }
