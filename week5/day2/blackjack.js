46af19c36852797e5fa484712fa7a2420932c4fa
Jessika Adams <jssklynn@gmail.com>
2022-12-15 07:58:38 -0700

    starting game logic
    suit values are tricky

--- week5/day2/blackjack.js
+++ week5/day2/blackjack.js
@@ -9,7 +9,7 @@ const deck = [];
 const playerCards = [];
 const dealerCards = [];
 const suits = ["hearts", "spades", "clubs", "diamonds"];
-const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
 
 const makeDeck = (rank, suit) => {
     const card = {
@@ -61,42 +61,75 @@ function dealHand () {
     dealDealer();
 }
 
-function hit () {
-    const hitCard = deck[Math.floor(Math.random()*deck.length)];
-    getCardImagePlayer(hitCard);
-    const dealerHit = deck[Math.floor(Math.random()*deck.length)];
-    getCardImageDealer(dealerHit)
+function hitPlayer () {
+    const playerCardOne = deck[Math.floor(Math.random()*deck.length)];
+    getCardImagePlayer(playerCardOne);
+    playerCards.push(playerCardOne)
     playerScore = calculatePointsPlayer(playerCards, playerCards.length)
+}
+
+function hitDealer () {
+    const dealerCardOne = deck[Math.floor(Math.random()*deck.length)];
+    getCardImageDealer(dealerCardOne);
+    dealerCards.push(dealerCardOne)
     dealerScore = calculatePointsDealer(dealerCards, dealerCards.length)
+}
+
+function hit() {
+    hitPlayer()
+    hitDealer()
 }
 
 function stand () {
     const dealerHit = deck[Math.floor(Math.random()*deck.length)];
     getCardImageDealer(dealerHit)
+    dealerCards.push(dealerCardOne)
+    dealerScore = calculatePointsDealer(dealerCards, dealerCards.length)
+}
+
+const faceCardPoints = (hand = [], score = 0) => {
+    for (const card of hand) {
+        switch (card.rank) {
+            case "ace":
+                if (score > 10) score += 1;
+                if (score <=10) score =+ 11;
+                break;
+            case "jack":
+                score += 10;
+                break;
+            case "queen":
+                score += 10;
+                break;
+            case "king":
+                score += 10;
+                break;
+        }
+    }
 }
 
 function calculatePointsPlayer (arr, size) {
-    let sum = 0;
+    let score = 0;
     for(let i=0; i<size;i++) {
-        sum += arr[i].pointValue;
+        score += arr[i].pointValue;
+        faceCardPoints(playerCards, 0);
     }
-    playerPoints.append(sum)
-    return sum
+    playerPoints.innerText = score
 }
 
 function calculatePointsDealer (arr, size) {
-    let sum = 0;
+    let score = 0;
     for(let i=0; i<size;i++) {
-        sum += arr[i].pointValue;
+        score += arr[i].pointValue;
+        faceCardPoints(dealerCards, 0);
     }
-    dealerPoints + sum
-    return sum
+    dealerPoints.innerText = score
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   // Execute after page load
 });
 
-dealButton.addEventListener("click", dealHand)
-hitButton.addEventListener("click", hit)
+dealButton.addEventListener("click", dealHand, calculatePointsDealer, calculatePointsPlayer)
+hitButton.addEventListener("click", hit, calculatePointsDealer, calculatePointsPlayer)
+standButton.addEventListener("click", stand, calculatePointsDealer, calculatePointsPlayer)
 
