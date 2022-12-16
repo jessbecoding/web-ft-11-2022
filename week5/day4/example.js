7bcf450a6c8a0bc202783c5a1e15cd736bc20f2a
Jessika Adams <118194134+jssklynn@users.noreply.github.com>
2022-12-16 13:18:09 -0700

    Update example.js

--- week5/day4/example.js
+++ week5/day4/example.js
@@ -1,15 +1,1 @@
-const searchButton = document.querySelector(".search");
-const mainContainer = document.querySelector(".main-container")
 
-const getWeather = async () => {
-    const weatherInput = document.querySelector(".weather").value;
-    const weatherData = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=${weatherInput}&appid=ff1cadec65d9f10c3aafc2a01b14612c`
-        );
-        const json = await weatherData.json();
-        const cityHeader = document.createElement("h1");
-        cityHeader.innerText = `It's ${json.main.temp} F in ${json.name}`;
-        mainContainer.append(cityHeader)
-};
-
-searchButton.addEventListener("click", getWeather);
