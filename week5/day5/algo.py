6d6afa5f211432799f28f0dcf44badf0ad86b44d
Jessika Adams <jssklynn@gmail.com>
2022-12-19 14:28:04 -0700

    movie page
    movie page

---
+++ week5/day5/algo.py
@@ -0,0 +1,15 @@
+string = ""
+
+def function():
+    userInput = input("Enter a sentance or word")
+    newString = userInput + string
+    arr = newString.split()
+    for word in arr:
+        letterCount = 0
+        letters = enumerate(word)
+        if letters == letters:
+            letterCount == +1
+        print(letterCount)
+
+
+function()
---
+++ week6/day1/movie.js
@@ -0,0 +1,44 @@
+const searchMovies = async () => {
+    const movieCardContainer = document.querySelector(".movieCardContainer");
+    movieCardContainer.innerHTML = "";
+    const inputField = document
+    .querySelector(".searchField")
+    .value.replace(/\s/g, "+");
+    const movieURL = `http://www.omdbapi.com/?apikey=########&s=${inputField}`;
+
+    const rawData = await fetch(movieURL);
+    const json = await rawData.json();
+
+    json.Search.forEach(async (movie) => {
+    const movieCard = document.createElement("div");
+      //img
+    const moviePoster = document.createElement("img");
+      //h2
+    const movieTitle = document.createElement("h2");
+      // p
+    const movieDescription = document.createElement("p");
+      // p
+    const movieYear = document.createElement("p");
+    const imdbID = movie.imdbID;
+    const individualMovieData = `http://www.omdbapi.com/?apikey=c2b8d3c5&i=${imdbID}`;
+    const fetchIndividualMovie = await fetch(individualMovieData);
+    const individualJson = await fetchIndividualMovie.json();
+      // modify
+    movieCard.classList = "movieCard";
+    moviePoster.classList = "moviePoster";
+    moviePoster.src = individualJson.Poster;
+    movieTitle.innerText = `${individualJson.Title} (${individualJson.Year})`;
+    movieDescription.innerText = individualJson.Plot;
+
+      // string interpolation
+
+      // append
+    movieCard.append(moviePoster, movieTitle, movieYear,movieDescription);
+    movieCardContainer.append(movieCard);
+    });
+    //div
+};
+
+const searchButton = document.querySelector(".searchButton");
+
+searchButton.addEventListener("click", searchMovies);
