0657cf318178199e2983870b4737e285e7e0c5a6
Jessika Adams <jssklynn@gmail.com>
2022-12-21 09:18:46 -0700

    algo  prac
    algo

--- week6/day3/sortedSquaredArray.js
+++ week6/day3/sortedSquaredArray.js
@@ -1,6 +1,6 @@
 // loop and square
 // sort ascending
-// return array (NOT CONSOLE LOG  ARRAY)
+// return array (NOT CONSOLE LOG ARRAY)
 
 array = [-3, 1, 2]
 newArray =[]
@@ -9,10 +9,8 @@ function squaredArray() {
     array.forEach(number => {
         const newNumber = Math.pow(number, 2);
     newArray.push(newNumber);
-    const sortedArray = newArray.sort((a,b) => (a-b))
-    return sortedArray;
     });
-    
+    return newArray.sort((a, b) => (a - b));
 }
 
 squaredArray()
