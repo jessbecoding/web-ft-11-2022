48e5c0a4b43f7d2c574e10b9cafcedccb0c1dd69
jssklynn <jssklynn@gmail.com>
2022-12-22 15:29:28 -0700

    A Few Things
    movie.js update to hide API key. front-end-proj got pet names to display as headers once zip input is received

---
+++ week6/front-end-proj/landingpage.js
@@ -0,0 +1,43 @@
+// API INFO ONLY CHANGE TOKEN
+
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer {TOKEN HERE}");
+
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+};
+
+// END API TOKEN
+
+const mainContainer = document.querySelector(".mainContainer")
+const zipSubmit = document.querySelector(".zipSubmit");
+
+const searchPetsByZip = async () => {
+    const petCardContainer = document.querySelector(".petCardContainer");
+    petCardContainer.innerHTML = "";
+    const zipInput = document.querySelector(".zipInput")
+    const petZipURL = `https://api.petfinder.com/v2/animals?location=${zipInput.value}`;
+    const petData = await fetch(petZipURL, requestOptions);
+    const json = await petData.json();
+    jsonHome = json.animals
+
+    
+    jsonHome.forEach(animal => {
+        // console.log(animal);
+        const petCard = document.createElement("div");
+        petCard.className = "petCard";
+        const petPhoto = document.createElement("img");
+        petPhoto.className = "petPhoto";
+        // petPhoto.src = animal.photos.medium
+        const petName = document.createElement("h1");
+        petName.className = "petName";
+        petName.innerText = animal.name
+        petCard.append(petName)
+        mainContainer.append(petCard)
+        // console.log(animal.photos.medium)
+    });
+}
+
+zipSubmit.addEventListener("click", searchPetsByZip);
