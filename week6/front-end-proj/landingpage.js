1120666b9ae41d6c2b0174996ffb1feeca5d7d95
jssklynn <jssklynn@gmail.com>
2023-01-03 16:13:09 -0700

    Bug Fixed
    Can search zip, form resets after each submit, images still not working

--- week6/front-end-proj/landingpage.js
+++ week6/front-end-proj/landingpage.js
@@ -16,27 +16,28 @@ const zipSubmit = document.querySelector(".zipSubmit");
 
 const searchPetsByZip = async () => {
     const petCardContainer = document.querySelector(".petCardContainer");
-    // petCardContainer.innerHTML = "";
+    petCardContainer.innerHTML = "";
     const zipInput = document.querySelector(".zipInput")
     const petZipURL = `https://api.petfinder.com/v2/animals?location=${zipInput.value}`;
+    console.log (petZipURL)
     const petData = await fetch(petZipURL, requestOptions);
     const json = await petData.json();
     jsonHome = json.animals
+    zipInput.value = null
 
-    
     jsonHome.forEach(animal => {
-        // console.log(animal);
         const petCard = document.createElement("div");
         petCard.className = "petCard";
         const petPhoto = document.createElement("img");
         petPhoto.className = "petPhoto";
         petPhoto.src = animal.photos.medium
+        petPhoto.onerror = ""
         const petName = document.createElement("h1");
         petName.className = "petName";
-        petName.innerText = animal.name
-        petCard.append(petName, petPhoto)
-        petCard.append(mainContainer)
-       
+        petName.innerText = animal.name;
+        petCard.append(petName, petPhoto);
+        petCardContainer.append(petCard);
+        mainContainer.append(petCardContainer)
     });
 }
 
