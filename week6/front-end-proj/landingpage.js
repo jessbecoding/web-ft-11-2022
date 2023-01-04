568f18d5ef3e9a6429070576e047951ace90029d
jssklynn <jssklynn@gmail.com>
2023-01-04 13:17:11 -0700

    Working on Pet Details
    Can click image and console log the link it leads to, need to work on opening new HTML

--- week6/front-end-proj/landingpage.js
+++ week6/front-end-proj/landingpage.js
@@ -21,19 +21,25 @@ const searchPetsByZip = async () => {
     const petZipURL = `https://api.petfinder.com/v2/animals/?location=${zipInput.value}`;
     const petData = await fetch(petZipURL, requestOptions);
     const json = await petData.json();
-    jsonHome = json.animals
-    zipInput.value = null
+    jsonHome = json.animals;
+    zipInput.value = null;
 
     jsonHome.forEach(animal => {
         const petCard = document.createElement("div");
         petCard.className = "petCard";
-        console.log(animal?.primary_photo_cropped?.small)
         const petPhoto = document.createElement("img");
         petPhoto.className = "petPhoto";
-        petPhoto.src = animal?.primary_photo_cropped?.small ? animal?.primary_photo_cropped?.small: "https://st2.depositphotos.com/12093440/44102/v/950/depositphotos_441020378-stock-illustration-page-404-link-to-non.jpg"
-        petPhoto.onclick = function() {
-            window.location.href = "petDetails.html"
-        }
+        petPhoto.src = animal?.primary_photo_cropped?.small ? animal?.primary_photo_cropped?.small: "https://st2.depositphotos.com/12093440/44102/v/950/depositphotos_441020378-stock-illustration-page-404-link-to-non.jpg";
+
+        const displayDetails = async () => {
+            petYouWant = animal.id;
+            const petDetailsURL = `GET https://api.petfinder.com/v2/animals/${petYouWant}`;
+            const detailPhoto = document.createElement("img");
+            detailPhoto.className = "petPhoto";
+            console.log (petDetailsURL);
+        };
+
+        petPhoto.addEventListener("click", displayDetails);
         const petName = document.createElement("h1");
         petName.className = "petName";
         petName.innerText = animal.name;
