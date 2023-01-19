fbc33dd196b8737b90aa179d5828bb7e314c5923
Jessika Adams <120666250+jessbecoding@users.noreply.github.com>
2023-01-19 10:09:14 -0700

    morning work

--- week9/day3/morning/index.js
+++ week9/day3/morning/index.js
@@ -1,60 +1,13 @@
 const express =  require('express')
 const app = express()
-const {Owners,Pets} = require("./models")
-const {Op} = require('sequelize')
+const petRoutes = require("./routes/petRoutes")
+const authRoutes = require("./routes/authRoutes")
 const PORT = 3008
 
 app.use(express.json());
 
-app.get("/get_pets", async (req, res) => {
-    const pets = await Pets.findAll();
-    res.send(pets)
-})
+app.use("/petClinic", petRoutes)
+app.use("/auth", authRoutes)
 
-app.get("/get_owners", async (req, res) => {
-    const owners = await Owners.findAll();
-    res.send(owners)
-})
 
-app.get("/find_Jessika", async (req, res) => {
-    const Jessika = await Owners.findAll({
-        where: {
-            firstName: "Jessika"
-        }
-    });
-    res.send(Jessika)
-})
-
-app.get("/find_Leia", async (req, res) => {
-    const Leia = await Pets.findAll({
-        where: {
-            name: "Leia"
-        }
-    });
-    res.send(Leia)
-})
-
-app.get("/get_petsID", async (req, res) => {
-    const pets = await Pets.findAll({
-        where: {
-            id: {
-                [Op.gte]: 2,
-                [Op.lte]: 4,
-            }
-        }
-    });
-    res.send(pets)
-})
-
-app.post("/create_pet", async (req, res) => {
-    const { name, type, age } = req.body;
-    const newPet = await Pets.create({
-        name,
-        type,
-        age,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    })
-    res.send(newPet)
-})
 app.listen(PORT, console.log(`listening on port ${PORT}`))
