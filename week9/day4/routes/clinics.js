886bc185dfb4c70cd4c967d2d5c67d0185ed4b33
Jessika Adams <120666250+jessbecoding@users.noreply.github.com>
2023-01-19 14:57:58 -0700

    eod

--- week9/day4/routes/clinics.js
+++ week9/day4/routes/clinics.js
@@ -22,7 +22,38 @@ router.get("/get_clinics", async (req, res) => {
 });
 
 // UPDATE
+router.put("/update_clinic", async (req, res) => {
+  const { id, clinicName, location, size } = req.body;
+  await Clinics.update(
+    {
+      clinicName: clinicName,
+      location: location,
+      size: size,
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  );
+  const updatedClinic = await Clinics.findAll({
+    where: {
+      id: id,
+    },
+  });
+  res.send(updatedClinic);
+});
 
 // DESTROY
+router.delete("/delete_clinic", async (req, res) => {
+  const { id } = req.body;
+  await Clinics.destroy({
+    where: {
+      id: id,
+    },
+  });
+  const allClinics = await Clinics.findAll();
+  res.send(allClinics);
+});
 
 module.exports = router;
--- week9/day4/routes/employees.js
+++ week9/day4/routes/employees.js
@@ -1,12 +1,59 @@
 const express = require("express");
 const router = express.Router();
 const { Employees } = require("../models");
+
 // CREATE
+router.post("/create_employee", async (req, res) => {
+  const { firstName, lastName, clinicId } = req.body;
+  const newEmployee = await Employees.create({
+    firstName,
+    lastName,
+    clinicId,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  res.send(newEmployee);
+});
 
 // READ
+router.get("/get_employees", async (req, res) => {
+  const allEmployees = await Employees.findAll();
+  res.send(allEmployees);
+});
 
 // UPDATE
+router.put("/update_employee", async (req, res) => {
+  const { firstName, lastName, clinicId } = req.body;
+  await Employees.update(
+    {
+      firstName: firstName,
+      lastName: lastName,
+      clinicId: clinicId,
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  );
+  const updatedEmployee = await Employees.findAll({
+    where: {
+      id: id,
+    },
+  });
+  res.send(updatedEmployee);
+});
 
 // DESTROY
+router.delete("/delete_employee", async (req, res) => {
+  const { id } = req.body;
+  await Employees.destroy({
+    where: {
+      id: id,
+    },
+  });
+  const allEmployees = await Employees.findAll();
+  res.send(allEmployees);
+});
 
 module.exports = router;
