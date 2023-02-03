ab280f868c69cb300dc4ce177ca01e7e21716274
jssklynn <jessikalynnadams@gmail.com>
2023-02-03 09:03:45 -0700

    algo warmup

---
+++ algo practice/2-3-2023.py
@@ -0,0 +1,12 @@
+# Remove duplicates from an array
+
+# array = [1, 2, 3, 4, 5, 3, 4]
+
+# def removeDups(arr):
+#     res = [*set(arr)]
+#     return res
+
+# removeDups(array)
+
+# Translate a phrase into Pig Latin.
+
