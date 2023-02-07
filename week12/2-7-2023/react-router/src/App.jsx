501c76c7be60aa37510630b60aa55ea9606ae453
Jessika Adams <120666250+jessbecoding@users.noreply.github.com>
2023-02-07 09:28:03 -0700

    working on nested routes

--- week12/2-7-2023/react-router/src/App.jsx
+++ week12/2-7-2023/react-router/src/App.jsx
@@ -1,10 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Account from "./components/Account/Account";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import Homepage from "./components/Homepage/Homepage";
+import Navbar from "./components/Navbar/Navbar";
+import AccountSettings from "./components/Account/AccountSettings";
+import AccountUser from "./components/Account/AccountUser";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Howdy</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/account">
+          <Route index element={<Account />} />
+          <Route path="settings" element={<AccountSettings />} />
+          <Route path=":id" element={<AccountUser />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
   );
 }
 
