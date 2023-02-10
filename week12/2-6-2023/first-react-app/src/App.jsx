fcea402d6271e2ea6d59744dd806519ee6e7b142
jssklynn <jessikalynnadams@gmail.com>
2023-02-09 17:52:28 -0700

    i give up :)

--- week12/2-6-2023/first-react-app/src/App.jsx
+++ week12/2-6-2023/first-react-app/src/App.jsx
@@ -5,7 +5,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>App.jsx</h1>
+    </div>
+  );
 }
 
 export default App;
